3473ebabecfa2f505e6f66f0862c32dca09e6881
김형기 <khk211113@naver.com>
2023-05-18 14:46:59 +0900

    feat: 판매글 작성 페이지 ui

--- app/src/main/java/com/lastminute/ui/main/MainActivity.kt
+++ app/src/main/java/com/lastminute/ui/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.lastminute.ui.main
 
 import android.os.Bundle
+import android.widget.Toast
 import com.lastminute.common.BaseActivity
 import com.lastminute.ui.R
 import com.lastminute.ui.databinding.ActivityMainBinding
+import com.lastminute.ui.post.PostFragment
 import com.lastminute.ui.search.SearchFragment
 import com.lastminute.util.changeFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -22,9 +24,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private fun initBottomNavigation() {
         binding.btNvMain.run {
             setOnItemSelectedListener { item ->
+                
                 when(item.itemId) {
                     R.id.navigation_search -> {
                         changeFragment(R.id.fragment_container_main, SearchFragment())
+                    }
+                    R.id.navigation_post -> {
+                        changeFragment(R.id.fragment_container_main, PostFragment())
                     }
                 }
                 true
---
+++ app/src/main/java/com/lastminute/ui/post/PostFragment.kt
@@ -0,0 +1,17 @@
+package com.lastminute.ui.post
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lastminute.common.BaseFragment
+import com.lastminute.ui.R
+import com.lastminute.ui.databinding.FragmentPostBinding
+
+class PostFragment() : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

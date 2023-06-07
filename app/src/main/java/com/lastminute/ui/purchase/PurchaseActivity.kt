210e9be293380823c1a8346b2316e4a0a191fbf0
김형기 <khk211113@naver.com>
2023-06-08 00:09:11 +0900

    feat: 구매내역 뷰

---
+++ app/src/main/java/com/lastminute/ui/purchase/PurchaseActivity.kt
@@ -0,0 +1,15 @@
+package com.lastminute.ui.purchase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lastminute.common.BaseActivity
+import com.lastminute.ui.R
+import com.lastminute.ui.databinding.ActivityPurchaseBinding
+
+class PurchaseActivity : BaseActivity<ActivityPurchaseBinding>(R.layout.activity_purchase) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
